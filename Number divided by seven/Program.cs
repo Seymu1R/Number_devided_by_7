ca900ea5dc451c1cef8d6c60b8bf713cafdeac32
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-18 16:56:05 +0400

    first commit

---
+++ Number divided by seven/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Number_divided_by_seven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, include number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int remainder;
+            if (num % 7 == 0)
+            {
+                Console.WriteLine("Number divided by seven");
+            }
+            else
+            {
+                remainder = num % 7;
+                if (remainder >= 4)
+                {
+                    Console.WriteLine("The nearest number divisible by seven:   " + ((num - remainder) + 7));
+                }
+                else if (0 <= remainder || remainder < 4)
+                {
+                    Console.WriteLine("The nearest number divisible by seven:  " + (num - remainder));
+                }
+
+
+
+
+            }
+            
+        }
+    }
+}
